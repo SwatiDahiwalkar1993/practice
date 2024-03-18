0903c49295028bc25145e05e958ee4ad3a2775df
admin <admin@DESKTOP-OIH9NHI>
2024-03-18 18:14:26 +0530

    Added

---
+++ src/main/java/CommanUtils/WebDriverUtil.java
@@ -0,0 +1,54 @@
+package CommanUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebDriverUtil {
+	public WebDriver driver;
+	public void maximize(WebDriver driver) {
+		driver.manage().window().maximize();
+	}
+public void implicitwait(WebDriver driver) {
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
+public void handleDropdown(WebElement element,String targatedelement) {
+	Select s = new Select(element);
+	s.selectByVisibleText(targatedelement);
+}
+public void mouseHover(WebDriver driver,WebElement element) {
+	Actions a = new Actions(driver);
+	a.moveToElement(element);
+	a.perform();
+	
+}
+public void switchwindow(WebDriver driver,String Expectedurl) {
+	Set<String> ids = driver.getWindowHandles();
+	for(String e: ids){
+	String actualurl = driver.switchTo().window(e).getCurrentUrl();
+		//String Chlildurl = "http://localhost:8888/index.php?module=Accounts&action=Popup&popuptype=specific_contact_account_address&form=TasksEditView&form_submit=false&fromlink=&recordid=";
+	if (actualurl. contains(Expectedurl)) {
+		break;
+	
+	}
+	}
+}
+  public String ScreenShot (WebDriver driver,String ScreenshotName) throws IOException {
+	TakesScreenshot ts =(TakesScreenshot) driver;
+ File tempfile = ts.getScreenshotAs(OutputType.FILE);
+ File destinationFile = new File("./Screenshot/"+ScreenshotName+".png");
+ FileUtils.copyFile(tempfile, destinationFile);
+// return destinationfile;
+ return destinationFile.getAbsolutePath();
+	
+  }
+}
--- src/test/java/Basic/OrganizationsTest.java
+++ src/test/java/Basic/OrganizationsTest.java
@@ -1,13 +1,43 @@
 package Basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import CommanUtils.PropertyFileUtil;
+import CommanUtils.WebDriverUtil;
+
 public class OrganizationsTest {
 	@Test 
-	public void CreateOrganizationsTest() {
-		WebDriver d = new ChromeDriver();
-		d.get("http://localhost:8888/");
-	}
+	public void OrganizationTest() throws IOException, InterruptedException
+	{
+	PropertyFileUtil putil = new PropertyFileUtil();
+	WebDriverUtil wutil = new WebDriverUtil();
+	WebDriver driver = new ChromeDriver();
+
+		// To read data from property file
+		String URL = putil.getDataFromPropertyFile("URL");
+		String USERNAME = putil.getDataFromPropertyFile("USERNAME");
+		String  PASSWORD= putil.getDataFromPropertyFile("PASSWORD");
+		
+		//To launch the web application
+		driver.get(URL);
+		//To Maximize the window
+				wutil.maximize(driver);
+				//To apply wait to findlElement()
+				wutil.implicitwait(driver);
+		
+		//To read data from Excel file
+		// String ORGNAME = eutil.getDataFromExcel("Orgnizations", 0, 1);
+	    // String GROUP = eutil.getDataFromExcel("Organizations", 1, 1);
+		 
+	     driver.get(URL);
+
+	    driver.findElement(By.name("user_name")).sendKeys(USERNAME);	
+	    driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+	    driver.findElement(By.id("submitButton")).click();
+}
 }
